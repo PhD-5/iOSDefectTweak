5a07f694fed91d3459c92c70c7006bc60e7fe409
yujianbo <745198699@qq.com>
2016-12-06 21:02:48 +0800

    [iOS缺陷检测综合版本 v1.2] 优化CCCryptHook模块

--- hooks/CCCryptHook.m
+++ hooks/CCCryptHook.m
@@ -2,19 +2,72 @@
 #import <CommonCrypto/CommonCryptor.h>
 #import "SocketClass.h"
 #import "CCCryptHook.h"
+#import "Utils.h"
+
+static size_t getIVLength(CCAlgorithm alg) {
+    switch(alg) {
+        case kCCAlgorithmAES128:
+            return kCCBlockSizeAES128;
+        case kCCAlgorithmDES:
+            return kCCBlockSizeDES;
+        case kCCAlgorithm3DES:
+            return kCCBlockSize3DES;
+        case kCCAlgorithmCAST:
+            return kCCBlockSizeCAST;
+        case kCCAlgorithmRC2:
+            return kCCBlockSizeRC2;
+        default:
+            return 0;
+    }
+}
+
+static NSString* getOperation(CCOperation op){
+    switch (op) {
+        case kCCEncrypt:
+            return @"Encrypt";
+        case kCCDecrypt:
+            return @"Decrypt";
+        default:
+            return @"unknown";
+    }
+}
+
+static NSString* getAlgName(CCAlgorithm alg){
+    switch (alg) {
+        case kCCAlgorithmAES128:
+            return @"AES128";
+        case kCCAlgorithmDES:
+            return @"DES";
+        case kCCAlgorithm3DES:
+            return @"3DES";
+        case kCCAlgorithmCAST:
+            return @"CAST";
+        case kCCAlgorithmRC4:
+            return @"RC4";
+        case kCCAlgorithmRC2:
+            return @"RC2";
+        default:
+            return @"unknown";
+    }
+}
+
+static NSString* getOption(CCOptions options){
+    switch (options) {
+        case 0:
+            return @"CBCMode";
+        case kCCOptionPKCS7Padding:
+            return @"PKCS7Padding | CBCMode";
+        case kCCOptionECBMode:
+            return @"ECBMode";
+        case 3:
+            return @"PKCS7Padding | ECBMode";
+        default:
+            return @"unknown";
+    }
+}
 
 
-//static NSString *preferenceFilePath = @"/private/var/mobile/Library/Preferences/com.softsec.iosdefect.plist";
-//
-//static BOOL getBoolFromPreferences(NSMutableDictionary *preferences, NSString *preferenceValue) {
-//    id value = [preferences objectForKey:preferenceValue];
-//    if (value == nil) {
-//        return YES; // default to YES
-//    }
-//    return [value boolValue];
-//}
-
-CCCryptorStatus (*original_CCCrypt)(
+static CCCryptorStatus (*original_CCCrypt)(
                         CCOperation op,         /* kCCEncrypt, etc. */
                         CCAlgorithm alg,        /* kCCAlgorithmAES128, etc. */
                         CCOptions options,      /* kCCOptionPKCS7Padding, etc. */
@@ -27,7 +80,7 @@ CCCryptorStatus (*original_CCCrypt)(
                         size_t dataOutAvailable,
                         size_t *dataOutMoved);
 
-CCCryptorStatus replaced_CCCrypt(
+static CCCryptorStatus replaced_CCCrypt(
                                  CCOperation op,         /* kCCEncrypt, etc. */
                                  CCAlgorithm alg,        /* kCCAlgorithmAES128, etc. */
                                  CCOptions options,      /* kCCOptionPKCS7Padding, etc. */
@@ -40,56 +93,93 @@ CCCryptorStatus replaced_CCCrypt(
                                  size_t dataOutAvailable,
                                  size_t *dataOutMoved)
 {
-    SocketClass *socket = [[SocketClass alloc] init];
 
-    // parse key datain and dataout
+    
+    ///////////////////////my code socket use string////////////////
     /*
+    //get key 
     NSData *keyData = [NSData dataWithBytes:key length:keyLength];
     NSString *keyStr = [[NSString alloc] initWithData:keyData encoding:NSUTF8StringEncoding]; 
-    
-    NSData *datainData = [NSData dataWithBytes:dataIn length:dataInLength];
-    NSString *datainStr = [[NSString alloc] initWithData:datainData encoding:NSUTF8StringEncoding];
-    
-    CCCryptorStatus result = original_CCCrypt(op,alg,options,key,keyLength,iv,dataIn,dataInLength,dataOut,dataOutAvailable,dataOutMoved);
-    
-    NSData *dataoutData = [NSData dataWithBytes:dataOut length:dataOutAvailable];
-    NSString *dataoutStr = [[NSString alloc] initWithData:dataoutData encoding:NSUTF8StringEncoding];
-    
-    NSString *string = [[NSString alloc] initWithFormat:@"op:%u alg:%u key:%@ inlen:%lu outlen:%lu datain:%@ dataout:%@ return:%d",
-                        op,alg,keyStr,dataInLength,dataOutAvailable,datainStr,dataoutStr,result];
-    [socket SendSocket:string];
-     */
-    NSData *keyData = [NSData dataWithBytes:key length:keyLength];
-    NSString *keyStr = [[NSString alloc] initWithData:keyData encoding:NSUTF8StringEncoding]; 
-    
+    //get input 
     NSData *inData = [NSData dataWithBytes:dataIn length:dataInLength];
     NSString *inStr = [[NSString alloc] initWithData:inData encoding:NSUTF8StringEncoding];
     if(inStr == nil){
-        inStr = @"null";
+        inStr = @"";
     }
-    
+    //get iv
+    NSString *ivStr;
+    if(iv == nil)
+        ivStr = @"";
+    else{
+        NSData *ivData = [NSData dataWithBytes:iv length:(unsigned int)getIVLength(alg)];
+        ivStr = [[NSString alloc] initWithData:ivData encoding:NSUTF8StringEncoding];
+    }
+    //call orig
+    CCCryptorStatus result = original_CCCrypt(op,alg,options,key,keyLength,iv,dataIn,dataInLength,dataOut,dataOutAvailable,dataOutMoved);
+    //get output
+    NSData *outData = [NSData dataWithBytes:dataOut length:(NSUInteger)*dataOutMoved];
+    NSString *outStr = [[NSString alloc] initWithData:outData encoding:NSUTF8StringEncoding];
+    if(outStr==nil){
+        outStr = @"";
+    }
+    //set socket data
     NSMutableDictionary *mDict = [[NSMutableDictionary alloc] init];
     [mDict setObject:@"CCCrypt" forKey:@"function"];
     [mDict setObject:[NSNumber numberWithInt:op] forKey:@"op"];
     [mDict setObject:[NSNumber numberWithInt:alg] forKey:@"alg"];
     [mDict setObject:[NSNumber numberWithInt:options] forKey:@"options"];
     [mDict setObject:keyStr forKey:@"key"];
+    [mDict setObject:ivStr forKey:@"iv"];
     [mDict setObject:inStr forKey:@"inputData"];
-    
-    CCCryptorStatus result = original_CCCrypt(op,alg,options,key,keyLength,iv,dataIn,dataInLength,dataOut,dataOutAvailable,dataOutMoved);
-    
-    NSData *outData = [NSData dataWithBytes:dataOut length:dataOutAvailable];
-    NSString *outStr = [[NSString alloc] initWithData:outData encoding:NSUTF8StringEncoding];
-    if(outStr==nil){
-        outStr = @"null";
-    }
-    
     [mDict setObject:outStr forKey:@"outputData"];
+    */
     
-//    if(![inStr isEqualToString:@"null"] && ![outStr isEqualToString:@"null"]){ 
+    NSMutableDictionary *mDict = [[NSMutableDictionary alloc] init];
+    [mDict setValue:@"CCCrypt" forKey:@"function"];
+//    if(op==0){ //encrypt
+        [mDict setValue:getOperation(op) forKey:@"op"];
+        [mDict setValue:getAlgName(alg) forKey:@"alg"];
+        [mDict setValue:getOption(options) forKey:@"options"];
+        
+        //get key
+        NSData *keyData = [Utils convertCBuffer:key withLength:keyLength];
+        [mDict setValue:[[NSString alloc] initWithData:keyData encoding:NSUTF8StringEncoding] forKey:@"key"];
+        
+        //get iv
+        NSData *ivData = [Utils convertCBuffer:iv withLength:getIVLength(alg)];
+        [mDict setValue:[[NSString alloc] initWithData:ivData encoding:NSUTF8StringEncoding] forKey:@"iv"];
+        
+        //get input data
+//        NSData *inputData = [Utils convertCBuffer:dataIn withLength:dataInLength];
+//        [mDict setValue:[[NSString alloc] initWithData:inputData encoding:NSUTF8StringEncoding] forKey:@"inputData"];
+        
+        //socket send
+        SocketClass *socket = [[SocketClass alloc] init];
         NSData * jsonData = [NSJSONSerialization dataWithJSONObject:mDict options:0 error:nil];  
         NSString * myString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding]; 
         [socket SendSocket:myString];
+//    }
+    
+    
+    
+    //call orig
+    CCCryptorStatus result = original_CCCrypt(op,alg,options,key,keyLength,iv,dataIn,dataInLength,dataOut,dataOutAvailable,dataOutMoved);
+//    [mDict setValue:[Utils convertCBuffer:dataOut withLength:*dataOutMoved] forKey:@"outputData"];
+//    [mDict setValue:[NSNumber numberWithInt:dataOutAvailable] forKey:@"dataOutAvailable"];
+    
+    
+//    if(![inStr isEqualToString:@"null"] && ![outStr isEqualToString:@"null"]){ 
+//    NSError *error;
+//    NSData *plist = [NSPropertyListSerialization dataWithPropertyList:(id)mDict
+//                                                               format:NSPropertyListXMLFormat_v1_0 
+//                                                              options:0
+//                                                                error:&error];
+//    NSString *plistStr = [[NSString alloc] initWithData:plist encoding:NSASCIIStringEncoding];
+    
+//    NSLog(@"yujianbo-----%@",myString);
+//    [mDict release];
+//    [myString release];
+//    [socket release];
 //    }
     return result;
 }
@@ -97,29 +187,7 @@ CCCryptorStatus replaced_CCCrypt(
 @implementation CCCryptHook
 
 + (void)enableHook{
-//    NSLog(@"yujianbo: Start hook cccrypt");
     MSHookFunction((void *) CCCrypt,(void *)  replaced_CCCrypt, (void **) &original_CCCrypt);  
 }
 
 @end
-
-//%ctor {NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-//    
-//    NSString *appId = [[NSBundle mainBundle] bundleIdentifier];
-//    // Load  preferences
-//    NSMutableDictionary *preferences = [[NSMutableDictionary alloc] initWithContentsOfFile:preferenceFilePath];
-//    id shouldHook = [preferences objectForKey:appId];
-//    if ( (shouldHook == nil) || (! [shouldHook boolValue]) ) {
-//        [preferences release];
-//        [pool drain];
-//        return;
-//    }
-//    
-//    if (getBoolFromPreferences(preferences, @"MITM")) {
-//        
-//    }
-//    
-//    [preferences release];
-//    [pool drain];
-//
-//}
